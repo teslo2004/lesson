a3c44e7c3dd05f4367a030c6ea4fba9a7de2fd2c
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-03-06 10:10:30 +0300

    second lesson

---
+++ lesson2/script1.js
@@ -0,0 +1,14 @@
+﻿var str1 = prompt("Введите первую строку: ");
+var str2 = prompt("Введите вторую строку: ");
+
+var arr1 = str1.split("");
+var arr2 = str2.split("");
+
+str1 = arr1.sort().join("");
+str2 = arr2.sort().join("");
+
+if(str1==str2){
+	alert("Строки являются анаграммами");
+}else{
+	alert("Строки не анаграммы");
+}
---
+++ lesson2/script2.js
@@ -0,0 +1,15 @@
+﻿var arr = prompt("Введите любые числа через пробел: ").split(" ");
+function sumNum(){
+var res =0;
+var num=0;
+
+for (var i in arr){
+	if (arr[i]%3==0){
+		res=res+(+arr[i]);
+		num=num+1;
+	}
+	
+}
+alert("Количество чисел = "+num+", их сумма = "+res);
+}
+sumNum();
