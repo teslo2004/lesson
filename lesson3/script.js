f7684fbd8d2130b85401e7ab87591dff07e0e1f3
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-03-08 00:35:12 +0300

    Update task 2

---
+++ lesson3/script.js
@@ -0,0 +1,24 @@
+﻿var p1 = -10;
+var p2 = 10;
+var percent = Math.floor(p1 + (p2 - p1)*Math.random());
+var p = (100-percent)/100;
+console.log(percent);
+console.log(p);
+
+var A = -500*p;
+var B = 500*p;
+
+var n = 10;
+
+var arr = new Array(n);
+arr.fill(0);
+for (var i in arr){
+	arr[i] = Math.floor(A + (B - A)*Math.random());
+}
+console.log(arr);
+
+
+arr.sort(function(a,b){
+	return Math.abs(b%10) - Math.abs(a%10);
+};
+console.log(arr);
