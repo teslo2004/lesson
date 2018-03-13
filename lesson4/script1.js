19f036f2aa823e8c34c72926bcade3669d1bd1ac
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-03-13 09:44:14 +0300

    Update script1.js

--- lesson4/script1.js
+++ lesson4/script1.js
@@ -1,10 +1,10 @@
-var t;
-function up() {
+var timer;
+function down() {
 if((window.innerHeight + window.pageYOffset) < document.documentElement.scrollHeight){ 
 window.scrollBy(0,100); 
-t = setTimeout('up()',200);
+timer = setTimeout(down(),200);
 } 
-else clearTimeout(t); 
+else clearTimeout(timer); 
 return false;
 }
-up();
+down();
