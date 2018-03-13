551184a71ed4a3690dc2869b1b3f3e84d0312302
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-03-13 09:45:36 +0300

    Update script1.js

--- lesson4/script1.js
+++ lesson4/script1.js
@@ -2,7 +2,7 @@ var timer;
 function down() {
 if((window.innerHeight + window.pageYOffset) < document.documentElement.scrollHeight){ 
 window.scrollBy(0,100); 
-timer = setTimeout(down(),200);
+timer = setTimeout(down,200);
 } 
 else clearTimeout(timer); 
 return false;
