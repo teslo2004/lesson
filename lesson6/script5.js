c66f3d722b4c53e4c5b146709b73ac3d6c1ce0d4
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-03-27 13:11:57 +0300

    task 5

--- lesson6/script5.js
+++ lesson6/script5.js
@@ -23,34 +23,15 @@ arr2 = [
 	tbody.appendChild(tr);
 	}
 
-
-
 var red=document.getElementsByTagName("td");
 for(var i=0;i<red.length;i++){
-	
 	red[i].addEventListener("mouseover", function(){
 		this.className='red';
 	});
 }
 
 for(var i=0;i<red.length;i++){
-	
 	red[i].addEventListener("mouseout", function(){
-		
 		this.className='red2';
-
-		})
-
+	})
 }
-
-
-
-
-
-//	timer=setTimeout(function(){			
-//		}
-//	},2000);
-//	};
-//	red[i].onmouseout=function(){
-//		clearTimeout(timer);
-//	};
