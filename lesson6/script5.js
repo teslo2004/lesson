81470d46d7455b02b3846cbabfd152bd742b0292
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-04-01 00:57:44 +0300

    task 5

--- lesson6/script5.js
+++ lesson6/script5.js
@@ -1,37 +1,44 @@
-arr2 = [
-		["Дмитрий", 33, "программист"],
-		["Виктор", 40, "водитель"], 
-		["Олег", 25, "журналист"],
-		["Игорь", 30, "футболист"]
-									];
-	var table = document.createElement("table");
-	document.body.appendChild(table);
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+	var n=+prompt("Введите размерность таблицы");
 
-	table.innerHTML="<tbody></tbody>";
-	table.setAttribute("border",2);
-	var tbody = table.children[0];
-	
-	for (var i in arr2){
-		var tr=document.createElement("tr");
-		var data=arr2[i];
-		var str="";
-		data.forEach(function(item){
-		str+="<td>"+item+"</td>";
-
-	});
-	tr.innerHTML=str;
-	tbody.appendChild(tr);
-	}
+	var table='<table border="3" width="760px">'
+       
+ 
+        for(var i = 1;i <= n;i++ ){
+ 
+            table += '<tr>';
+ 
+            for(var j = 1;j <= n;j++ ){
+                table += '<td>Таблица N*N</td>';
+            }
+ 
+            table += '</tr>';
+        }
+        table += '</table>';
+        document.getElementById('table').innerHTML = table;
+ 
 
 var red=document.getElementsByTagName("td");
 for(var i=0;i<red.length;i++){
+	
 	red[i].addEventListener("mouseover", function(){
 		this.className='red';
 	});
 }
 
 for(var i=0;i<red.length;i++){
+	
 	red[i].addEventListener("mouseout", function(){
+		
 		this.className='red2';
-	})
+
+		})
+
 }
+
+
+})
+
+
+
