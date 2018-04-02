6bbeb329c70a33ac5b4c8392e90d3ea45cbafd07
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2018-04-02 23:06:21 +0300

    task2

---
+++ lesson7/script2.js
@@ -0,0 +1,16 @@
+var mail = document.getElementById("mail");
+mail.addEventListener("keydown", function(e) {
+  var chars = e.key;
+  var value = this.value;
+  var reg = /[a-zA-Z0-9\.\-\_@a-z0-9\-\_*\.a-z]/;
+  if (!chars.match(reg)) {
+    e.preventDefault();
+  }
+  if (value.match(/@/) && chars=="@") {
+    e.preventDefault();
+  }
+  if (value[value.length-1]=="." && chars==".") {
+    e.preventDefault();
+  }
+});
+
